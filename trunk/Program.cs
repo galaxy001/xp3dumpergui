67a7a9b38b63a2794104928c132b55f1d2c51b11
xupefei@gmail.com <xupefei@gmail.com@745c8c0e-7648-cef9-7dd5-19cbee664a85>
2011-01-31 12:32:17 +0000

    r6

---
+++ trunk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Clowwindy.XP3Dumper.GUI;
+
+namespace xp3dumper_gui
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
---
+++ trunk/Utils/Win32APIUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Clowwindy.XP3Dumper.Utils
+{
+    internal static class Win32APIUtils
+    {
+        static Dictionary<string, int> dictNameToCodePage = new Dictionary<string, int>()
+        {
+            {"擔杮岅",932},//Japanese
+            {"いゅ(羉砰)",950},//Trad. Chs.
+            {"English",1252},//English
+            {"中文(简体)",936},//Simp. Chs.
+        };
+
+        /// <summary>
+        /// GetLocaleInfo API
+        /// </summary>
+        /// <param name="d">LCID</param>
+        /// <param name="s">LCTYPE</param>
+        /// <param name="k">CodePageString</param>
+        /// <param name="dd">SizeOf(CodePageString)</param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        private static extern int GetLocaleInfo(int d, int s, StringBuilder k, int dd);
+
+        /// <summary>
+        /// Get Codepage id form LCID
+        /// </summary>
+        /// <param name="lcid">LCID</param>
+        /// <returns>Codepage</returns>
+        internal static int GetLocaleCP(int lcid)
+        {
+            StringBuilder S = new StringBuilder(255);
+            int LCTYPE = 4;
+
+            int d = GetLocaleInfo(lcid, LCTYPE, S, 255);
+
+            int ret = 932;
+            try
+            {
+                ret = dictNameToCodePage[S.ToString()];
+            }
+            catch (Exception) { }
+
+            return ret;
+        }
+    }
+}
